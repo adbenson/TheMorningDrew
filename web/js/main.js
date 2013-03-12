ee07f6c0fb9e4e125e137cfd324344ca8e4cd440
Drew Benson <abenson@systemsinmotion.com>
2013-03-12 14:50:12 -0400

    Adding backdrop recalcuation logic

--- web/js/main.js
+++ web/js/main.js
@@ -9,6 +9,33 @@ backdrop_image.src = 'img/tautropfen3_720.jpg';
 
 $(document).ready(function() {
 
-});
 
+	$(window).resize(function() {
+		var window_ratio = window.innerWidth / window.innerHeight;
+		
+		var bd_width = backdrop_image.width;
+		var bd_height = backdrop_image.height;
+		
+		if (window_ratio > backdrop_ratio) {
+			//Window is wider than image
+			//stretch width to fit
+			
+			bd_width = window.innerWidth;
+			bd_height = bd_width / backdrop_ratio;
+			
+		}
+		else {
+			//Image is wider than window
+			//stretch height to fit
+			
+			bd_height = window.innerHeight;
+			bd_width = 	bd_height * backdrop_ratio;
+		}
+	
+		var bg_size = bd_width + 'px ' + bd_height + 'px';
+		
+		$('#backdrop').css('background-size', bg_size);
+	});
+
+});
 
