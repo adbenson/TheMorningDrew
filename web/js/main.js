0a98f8c40ee3bd973333acc8028a2c54d2f2c4a4
Drew Benson <abenson@systemsinmotion.com>
2013-03-12 14:49:27 -0400

    Adding backdrop pre-load and ratio calculation

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,12 @@
+var backdrop_image = new Image();
+var backdrop_ratio = null;
+
+backdrop_image.onload = function () {
+	backdrop_ratio = backdrop_image.width / backdrop_image.height;
+};
+
+backdrop_image.src = 'img/tautropfen3_720.jpg';
+
 $(document).ready(function() {
 
 });
