52c550b4d4e6fb44f80c71a0f76d0a5bb7f9ee5a
Drew Benson <abenson@systemsinmotion.com>
2013-03-12 16:59:44 -0400

    Refactoring for cleanliness, exporting functions

--- web/js/main.js
+++ web/js/main.js
@@ -1,47 +1,51 @@
 var backdrop_image = new Image();
-var backdrop_ratio = null;
+var backdrop_ratio = 1;
 
-backdrop_image.onload = function () {
-	backdrop_ratio = backdrop_image.width / backdrop_image.height;
-	$('#backdrop').show();
-	$(window).resize();
-};
-
+backdrop_image.onload = backdrop_onload;
 backdrop_image.src = 'img/tautropfen3_720.jpg';
 
 $(document).ready(function() {
 
-	$(window).resize(function() {
-		
-		var window_ratio = window.innerWidth / window.innerHeight;
-		
-		var bd_width = backdrop_image.width;
-		var bd_height = backdrop_image.height;
-		
-		if (window_ratio > backdrop_ratio) {
-			//Window is wider than image
-			//stretch width to fit
-			
-			bd_width = window.innerWidth;
-			bd_height = bd_width / backdrop_ratio;
-			
-		}
-		else {
-			//Image is wider than window
-			//stretch height to fit
-			
-			bd_height = window.innerHeight;
-			bd_width = 	bd_height * backdrop_ratio;
-		}
-	
-		var bg_size = bd_width + 'px ' + bd_height + 'px';
-		
-		$('#backdrop').css('background-size', bg_size);
-	});
+	$(window).resize(resize_backdrop);
 	
 	$('.attribution').click(function() {
 		$(this).find('div').slideToggle();
 	});
 	
 });
+
+function backdrop_onload() {
+	backdrop_ratio = backdrop_image.width / backdrop_image.height;
+	$('#backdrop').show();
+	$(window).resize();
+}
+
+function resize_backdrop() {
+
+	var window_ratio = window.innerWidth / window.innerHeight;
+	
+	var bd_width = backdrop_image.width;
+	var bd_height = backdrop_image.height;
+	
+	if (window_ratio > backdrop_ratio) {
+		//Window is wider than image
+		//stretch width to fit
+		
+		bd_width = window.innerWidth;
+		bd_height = bd_width / backdrop_ratio;
+		
+	}
+	else {
+		//Image is wider than window
+		//stretch height to fit
+		
+		bd_height = window.innerHeight;
+		bd_width = 	bd_height * backdrop_ratio;
+	}
+
+	var bg_size = bd_width + 'px ' + bd_height + 'px';
+	
+	$('#backdrop').css('background-size', bg_size);
+
+}
 
